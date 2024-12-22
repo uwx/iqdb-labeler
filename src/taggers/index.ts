2ce1df4d290c9c61a1c6b18fbf408ef0ce1138e4
uwx <13633343+uwx@users.noreply.github.com>
2024-12-22 10:33:52 -0300

    Add wdv3 tagger to matchers

--- src/taggers/index.ts
+++ src/taggers/index.ts
@@ -2,9 +2,11 @@
 import { HuggingFaceMatcher } from "./huggingface-wd-tagger/index.js";
 import { IqdbMatcher } from "./iqdb/index.js";
 import { Matcher } from "./matcher.js";
+import { WDv3Matcher } from "./wd-vit-v3/index.js";
 
 export const matchers: Matcher[] = [
-    new IqdbMatcher(),
-    new HuggingFaceMatcher(),
+    new WDv3Matcher(),
+    // new IqdbMatcher(),
+    // new HuggingFaceMatcher(),
     // new DeepDanbooruMatcher()
 ];
