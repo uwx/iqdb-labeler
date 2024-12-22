40cc07e7ee3f19449c6d4d349752f7471f6e2c9e
uwx <13633343+uwx@users.noreply.github.com>
2024-12-22 10:33:46 -0300

    Add feed-helpers.ts (currently unused)

---
+++ src/utils/feed-helpers.ts
@@ -0,0 +1,11 @@
+import { getLabelIdForTag, parseLabelIdentifier } from "../labels/index.js";
+import { alphabetParseInt, alphabetToString } from "./ints.js";
+
+export function getFeedRkeyFromLabelIdentifier(labelIdentifier: string) {
+    return alphabetToString(parseLabelIdentifier(labelIdentifier)[0]);
+}
+
+export function getLabelIdentifierFromFeedRkey(rkey: string) {
+    if (/[^a-z]/.test(rkey)) throw new Error('rkey is not valid! contains characters outside the base-26 alphabet');
+    return getLabelIdForTag(alphabetParseInt(rkey));
+}
