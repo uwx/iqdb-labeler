88887f249a26da874fab7e87d063bca800e0da34
deliciously-default <181034365+deliciously-default@users.noreply.github.com>
2026-02-24 15:51:07 +0100

    restore wd matcher

--- src/taggers/index.ts
+++ src/taggers/index.ts
@@ -1,8 +1,8 @@
-import { CamieMatcher } from "./camie-v2/index.js";
+// import { CamieMatcher } from "./camie-v2/index.js";
 import { Matcher } from "./matcher.js";
-// import { WdMatcher } from "./wd-swinv2-tagger-v3/index.js";
+import { WdMatcher } from "./wd-swinv2-tagger-v3/index.js";
 
 export const matchers: Matcher[] = [
-    // new WdMatcher(),
-    new CamieMatcher(),
+    new WdMatcher(),
+    // new CamieMatcher(),
 ];
